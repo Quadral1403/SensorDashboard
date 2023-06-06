fae4cea7a8f715fcde4e6c64f6caf21b25bcee12
quadral1403 <moritz.tomeric@gmail.com>
2023-06-06 16:46:11 +0200

    Grafana integrated

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  url: string = "http://localhost:3000/d-solo/a00a02d1-67c6-42b7-b8de-7881f26ea90a/altkleidercontainer?orgId=1&refresh=1m&theme=light&panelId=2";
+  urlSafe: SafeResourceUrl = "";
 
-  constructor() { }
+  constructor(public sanitizer: DomSanitizer) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 
 }
