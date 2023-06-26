875e1a40f70b99052695690311b20cb2bc11b6cd
quareal1403 <moritz.tomeric@gmail.com>
2023-06-26 15:46:28 +0200

    Implement Strundesensor iframe

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,13 +7,17 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  url: string = "http://localhost:3000/d-solo/a00a02d1-67c6-42b7-b8de-7881f26ea90a/altkleidercontainer?orgId=1&refresh=1m&theme=light&panelId=2";
-  urlSafe: SafeResourceUrl = "";
+  url1: string = "http://localhost:3000/d-solo/a00a02d1-67c6-42b7-b8de-7881f26ea90a/altkleidercontainer?orgId=1&refresh=1m&theme=light&panelId=2";
+  url2: string = "http://localhost:3000/d-solo/a3280554-9570-4307-b9a5-ba97bfc01ce4/strundesensorik?orgId=1&from=1687774595085&to=1687785395085&theme=light&panelId=1";
+  
+  urlSafe1: SafeResourceUrl = "";
+  urlSafe2: SafeResourceUrl = "";
 
   constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+    this.urlSafe1= this.sanitizer.bypassSecurityTrustResourceUrl(this.url1);
+    this.urlSafe2= this.sanitizer.bypassSecurityTrustResourceUrl(this.url2);
   }
 
 }
